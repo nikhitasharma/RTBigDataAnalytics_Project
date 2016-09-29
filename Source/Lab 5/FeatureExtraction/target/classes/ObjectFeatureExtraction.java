2207f66fb7ca259a270e23dea15fb7fb6f2aec39
ganeshtaduri <lakshmiganeshtaduri@gmail.com>
2016-09-28 23:37:55 -0500

    Tutorial 5

---
+++ Source/Lab 5/FeatureExtraction/target/classes/ObjectFeatureExtraction.java
@@ -0,0 +1,32 @@
+import org.openimaj.feature.local.list.LocalFeatureList;
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.MBFImage;
+import org.openimaj.image.feature.local.engine.DoGSIFTEngine;
+import org.openimaj.image.feature.local.keypoints.Keypoint;
+
+import java.io.*;
+
+
+public class ObjectFeatureExtraction {
+    public static void main(String args[]) throws IOException {
+        String inputFolder = "data/";
+        String inputImage = "aparna.png";
+        String outputFolder = "output/";
+        String[] IMAGE_CATEGORIES = {"Nikhitha", "Aparna", "Harsha", "Ganesh"};
+        int input_class = 1;
+        MBFImage mbfImage = ImageUtilities.readMBF(new File(inputFolder + inputImage));
+        DoGSIFTEngine doGSIFTEngine = new DoGSIFTEngine();
+        LocalFeatureList<Keypoint> features = doGSIFTEngine.findFeatures(mbfImage.flatten());
+        FileWriter fw = new FileWriter(outputFolder + IMAGE_CATEGORIES[input_class] + ".txt");
+        BufferedWriter bw = new BufferedWriter(fw);
+        for (int i = 0; i < features.size(); i++) {
+            double c[] = features.get(i).getFeatureVector().asDoubleVector();
+            bw.write(input_class + ",");
+            for (int j = 0; j < c.length; j++) {
+                bw.write(c[j] + " ");
+            }
+            bw.newLine();
+        }
+        bw.close();
+    }
+}
